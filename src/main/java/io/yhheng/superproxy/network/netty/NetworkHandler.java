f7e85bc53a22fb571754236923994863bfccf796
hengyoush <hengyoush1@163.com>
2020-11-01 09:11:52 +0800

    添加日志，将public方法改为private

--- src/main/java/io/yhheng/superproxy/network/netty/NetworkHandler.java
+++ src/main/java/io/yhheng/superproxy/network/netty/NetworkHandler.java
@@ -8,6 +8,8 @@ import io.yhheng.superproxy.network.FilterStatus;
 import io.yhheng.superproxy.network.Listener;
 import io.yhheng.superproxy.network.NetworkReadFilter;
 import io.yhheng.superproxy.protocol.DecodeResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +19,8 @@ import static io.yhheng.superproxy.network.ConnectionEventListener.ConnectionEve
 import static io.yhheng.superproxy.network.ConnectionEventListener.ConnectionEvent.RemoteClose;
 
 public class NetworkHandler extends ChannelDuplexHandler {
+    private static final Logger log = LoggerFactory.getLogger(NetworkHandler.class);
+
     private final Listener listener;
     private final Map<String, Connection> connTable = new ConcurrentHashMap<>();
 
@@ -26,6 +30,7 @@ public class NetworkHandler extends ChannelDuplexHandler {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        log.debug("处理数据，channel:{},id:{},\nmsg:{}", ctx.channel(), ctx.channel().id().asLongText(), msg.toString());
         if (msg instanceof DecodeResult) {
             handleData((DecodeResult) msg, connTable.get(ctx.channel().id().asLongText()));
         } else {
@@ -37,18 +42,20 @@ public class NetworkHandler extends ChannelDuplexHandler {
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         // TODO new Connection
         Connection connection = null;
+        log.info("新连接建立，channel:{},id:{}", ctx.channel(), ctx.channel().id().asLongText());
         handleNewConnection(connection);
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        log.info("连接断开，channel:{},id:{}", ctx.channel(), ctx.channel().id().asLongText());
         Connection removedConnection = connTable.remove(ctx.channel().id().asLongText());
         removedConnection.close();
         handleCloseConnection(removedConnection);
         ctx.fireChannelInactive();
     }
 
-    public void handleData(DecodeResult decodeResult, Connection connection) {
+    private void handleData(DecodeResult decodeResult, Connection connection) {
         List<NetworkReadFilter> networkReadFilters = listener.networkReadFilters();
         for (int i = 0; i < networkReadFilters.size(); i++) {
             FilterStatus filterStatus = networkReadFilters.get(i).onRead(decodeResult, connection);
@@ -58,7 +65,7 @@ public class NetworkHandler extends ChannelDuplexHandler {
         }
     }
 
-    public void handleNewConnection(Connection connection) {
+    private void handleNewConnection(Connection connection) {
         List<NetworkReadFilter> networkReadFilters = listener.networkReadFilters();
         List<ConnectionEventListener> connectionEventListeners = listener.connectionEventListeners();
         connTable.put(connection.id(), connection);
@@ -75,7 +82,7 @@ public class NetworkHandler extends ChannelDuplexHandler {
         }
     }
 
-    public void handleCloseConnection(Connection connection) {
+    private void handleCloseConnection(Connection connection) {
         List<ConnectionEventListener> connectionEventListeners = listener.connectionEventListeners();
         for (var l : connectionEventListeners) {
             l.onEvent(RemoteClose, connection);
